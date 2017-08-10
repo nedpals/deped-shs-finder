a2a8dcce5c2271c001ad498357f2ec6e499e8b63
Ned Palacios <npdoesmc@gmail.com>
2017-08-10 22:01:10 +0800

    Adding some styling

--- client/components/List.vue
+++ client/components/List.vue
@@ -1,16 +1,16 @@
 <template>
 <div>
-	<div v-if="listData" class="dib pv2 pv2-ns ba bb-0 br2">
-		<div v-bind:key="school.school_id" class="fl w-100 ph3 ph4-ns bb" v-for="school in listData">
-			<div class="fl w-70 pa1">
-				<h3 class="measure-narrow">{{ school.school_name }}</h3>
-				<p>{{ school.region }} / {{ region_fixed(school.region, school.province) }} / {{ school.municipality }}</p>
-				<ul class="pl0 list">
-					<li class="dib mr2 f5 lh-copy" v-for="p in school.programs">{{ p }}</li>
+	<div v-if="listData" class="dib b--navy ba bb-0 br3">
+		<div v-bind:key="school.school_id" class="fl w-100 pv1 ph2-ns bb" v-for="school in listData">
+			<div class="fl w-70 pa3">
+				<h3 class="f3 measure-narrow dark-blue ma0 mb2">{{ school.school_name }}</h3>
+				<p class="f4 navy mv1">{{ school.region }} / {{ region_fixed(school.region, school.province) }} / {{ school.municipality }}</p>
+				<ul class="pl0 ma0 list">
+					<li class="dib pa2 br2 bg-navy white mr2 f5" v-for="p in school.programs">{{ p }}</li>
 				</ul>
 			</div>
-			<div class="fl w-30 pa1">
-				<ul class="pl0 list">
+			<div v-show="typeof school.tvl_specializations !== 'undefined' && school.tvl_specializations.length > 0" class="fl w-30 bg-blue white br3 pa3">
+				<ul class="pl0 ma0 list">
 					<li class="f5 lh-copy" v-for="s in school.tvl_specializations">{{ s }}</li>
 				</ul>
 			</div>
--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,31 +1,36 @@
 <template>
-  <div class="pa3 pa4-ns">
-  	<nav>
-	  <a class="link dim black b f1 f-headline-ns tc db mb3 mb4-ns" href="#" title="Home">Site Name</a>
+<div>
+  <nav class="bg-blue pa3 pa4-ns">
+	 <a class="link dim white b f1 f-headline-ns tl db mb3 mb4-ns" href="#" title="Home">Site Name</a>
 	  <div class="tc pb3">
-	    <a class="link dim gray f6 f5-ns dib mr3" href="#" title="Home">Home</a>
-	    <a class="link dim gray f6 f5-ns dib mr3" href="#" title="About">About</a>
-	    <a class="link dim gray f6 f5-ns dib mr3" href="#" title="Store">Store</a>
-	    <a class="link dim gray f6 f5-ns dib" href="#" title="Contact">Contact</a>
+	    <a class="link dim white f6 f5-ns dib mr3" href="#" title="Home">Home</a>
+	    <a class="link dim white f6 f5-ns dib mr3" href="#" title="About">About</a>
+	    <a class="link dim white f6 f5-ns dib mr3" href="#" title="Store">Store</a>
+	    <a class="link dim white f6 f5-ns dib" href="#" title="Contact">Contact</a>
 	  </div>
-	</nav>
-	<div class="fl w-100 pv3">
-	  	<div class="fl w-20">
-	  		<select>
-	  			<option v-for="r in regions" :value="r">{{ r }}</option>
-	  		</select>
-	  	</div>
-	  	<div class="fl w-20">
-	  		<input type="text" />
-	  	</div>
-	  	<div class="fl w-20">
-	  		<select>
-	  			<option v-for="p in programs" :value="p">{{ p }}</option>
-	  		</select>
-	  	</div>
-	  </div>
+  </nav>
+  <div class="f3 fl w-100 pv3 mb5 dib bg-dark-blue pa3 pa4-ns">
+  	<div class="fl w-20 pr4">
+  		<select class="w-100 bg-navy pv1 ph2 b--none white br2">
+  			<option v-for="r in regions" :value="r">{{ r }}</option>
+  		</select>
+  	</div>
+  	<div class="fl w-30 pr3">
+  		<input class="pa2 w-100 bg-navy b--none white br2" type="text" />
+  	</div>
+  	<div class="fl w-20 pr3">
+  		<select class="bg-navy pv1 ph2 b--none white br2">
+  			<option v-for="p in programs" :value="p">{{ p }}</option>
+  		</select>
+  	</div>
+  </div>
+  <div class="pa3 pa4-ns">
     <list v-infinite-scroll="loadMore" infinite-scroll-disabled="isLoading" infinite-scroll-distance="10" v-bind:loading="isLoading" v-bind:list-data="schoolData"></list>
   </div>
+  <footer class="dib w-100 ph4 fixed left-0 right-0 bottom-0 bg-navy white">
+  	<p class="f5">&copy; 2017 </p>
+  </footer>
+</div>
 </template>
 
 <style src="tachyons/css/tachyons.css"></style>
