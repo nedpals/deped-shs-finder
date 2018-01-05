abfa3119a527d256fb9cd75b020cd89a846bb625
Ned Palacios <npdoesmc@gmail.com>
2018-01-05 20:35:17 +0800

    updated store

--- client/store/index.js
+++ client/store/index.js
@@ -7,11 +7,12 @@ Vue.use(Vuex)
 
 const state = {
   search: {
-    query: '',
+    q: '',
     region: '',
     province: '',
     municipality: '',
-    programs: []
+    programs: [],
+    results: []
   },
   schools: [],
   regions: [],
@@ -24,12 +25,19 @@ const state = {
     'TVL',
     'Arts and Design',
     'Sports'
-  ]
+  ],
+  selected_school: []
 }
 
 const mutations = {
   LOAD (state, data) {
     state.schools.push(...data)
+  },
+  LOAD_SCHOOL (state, data) {
+    state.selected_school = data
+  },
+  LOAD_RESULT (state, data) {
+    state.search.results = data
   }
 }
 
@@ -42,13 +50,36 @@ const actions = {
       .catch((error) => {
         console.log(error)
       })
+  },
+  findSchoolById ({ commit }, schoolId) {
+    fetchApi().get('/schools', { params: { school_id: schoolId } })
+      .then((response) => {
+        commit('LOAD_SCHOOL', response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+  searchSchool ({ commit }, searchParams) {
+    searchParams = Object.keys(searchParams).forEach((key) => (searchParams[key] == null) && delete searchParams[key])
+    fetchApi().get('/schools', { params: searchParams })
+      .then((response) => {
+        commit('LOAD_RESULT', response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 }
 
 const getters = {
   limitData: (state, getters) => (num) => {
     return state.schools.slice(0, num)
-  }
+  },
+  // selectedSchool: (state, getters) => (id) => {
+  //   return
+  //   // return state.schools.find(school => school.school_id === id)
+  // }
 }
 
 const store = new Vuex.Store({
